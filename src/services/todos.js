2cde6844574456b3b18e9062d60a507a61a52430
Tonthowi Al Ahyar <tonthowi@gmail.com>
2024-07-13 10:01:02 +0700

    add backend functionality

--- src/services/todos.js
+++ src/services/todos.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3000/todos'
+const baseUrl = 'https://todo-list-app-backend-red.vercel.app/todos'
 
 const getAllTodos = () => {
     const request = axios.get(baseUrl)
